475a4931c5240b1ddaf65b4f4f799715c01bd8c5
David <dodwei.dev@gmail.com>
2019-06-28 09:13:21 +0800

    #9 - Intro to Hooks

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>This wild darkness</li>
+                <li>memory gospel</li>
+            </ul>
+        </div>
+    )
+}
+
+export default SongList;
